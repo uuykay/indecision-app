a191f55c2033e892e756f5f898c55f75779bf505
Will <william.y.kuang@gmail.com>
2018-04-25 23:00:07 +1000

    Templating. 011

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,11 +2,68 @@
 
 console.log("App.js is running");
 
+var app = {
+  title: "Indecision App",
+  subtitle: "Put your life in the hands of a computer"
+};
+
 var template = React.createElement(
-  "p",
+  "div",
   null,
-  "Does this change?"
+  React.createElement(
+    "h1",
+    null,
+    app.title
+  ),
+  React.createElement(
+    "p",
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "1"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "2"
+    )
+  )
 );
+
+var user = {
+  name: "William Kuang",
+  age: 27,
+  location: "Sydney"
+};
+
+var template2 = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    user.name
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Age: ",
+    user.age
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Location: ",
+    user.location
+  )
+);
+
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,35 @@
 console.log("App.js is running");
 
-var template = <p>Does this change?</p>;
+var app = {
+  title: "Indecision App",
+  subtitle: "Put your life in the hands of a computer"
+};
+
+var template = (
+  <div>
+    <h1>{app.title}</h1>
+    <p>{app.subtitle}</p>
+    <ol>
+      <li>1</li>
+      <li>2</li>
+    </ol>
+  </div>
+);
+
+var user = {
+  name: "William Kuang",
+  age: 27,
+  location: "Sydney"
+};
+
+var template2 = (
+  <div>
+    <h1>{user.name}</h1>
+    <p>Age: {user.age}</p>
+    <p>Location: {user.location}</p>
+  </div>
+);
+
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
