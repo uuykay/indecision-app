b2768d92271b7b1bf799d5b019e3d5ae3a5c04a9
Will <william.y.kuang@gmail.com>
2018-06-26 10:17:28 +1000

    Converted some classes to functional components. V40

--- src/app.js
+++ src/app.js
@@ -64,48 +64,37 @@ class IndecisionApp extends React.Component {
   }
 }
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Indecision</h1>
-        <h2>Put your life in the hands of a computer</h2>
-      </div>
-    );
-  }
-}
+const Header = () => {
+  return (
+    <div>
+      <h1>Indecision</h1>
+      <h2>Put your life in the hands of a computer</h2>
+    </div>
+  );
+};
 
-class Action extends React.Component {
-  render() {
-    return (
-      <div>
-        <button onClick={this.props.handlePick} disabled={!this.props.hasOptions}>
-          What should I do?
-        </button>
-      </div>
-    );
-  }
-}
+const Action = props => {
+  return (
+    <div>
+      <button onClick={props.handlePick} disabled={!props.hasOptions}>
+        What should I do?
+      </button>
+    </div>
+  );
+};
 
-// Options -> Options component here
-class Options extends React.Component {
-  render() {
-    const { options, handleDeleteOptions } = this.props;
-    return (
-      <div>
-        <button onClick={handleDeleteOptions}>Remove All</button>
-        <ul>{options.map((option, i) => <Option option={option} key={i} />)}</ul>
-      </div>
-    );
-  }
-}
-
-class Option extends React.Component {
-  render() {
-    const { option } = this.props;
-    return <li>{option}</li>;
-  }
-}
+const Options = props => {
+  const { options, handleDeleteOptions } = props;
+  return (
+    <div>
+      <button onClick={handleDeleteOptions}>Remove All</button>
+      <ul>{options.map((option, i) => <Option option={option} key={i} />)}</ul>
+    </div>
+  );
+};
+const Option = props => {
+  return <li>{props.option}</li>;
+};
 
 // AddOption -> AddOption component here
 class AddOption extends React.Component {
